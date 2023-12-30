2d106642e6036b33e33f79b5c9f608576f2e6804
Sebastiaan Delodder <sebastiaan.delodder@student.hogent.be>
2023-12-30 23:38:34 +0100

    Landscape and portrait mode

--- app/src/main/java/com/example/metmuseum/MainActivity.kt
+++ app/src/main/java/com/example/metmuseum/MainActivity.kt
@@ -5,55 +5,63 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.metmuseum.ui.artScreen.ArtScreen
 import com.example.metmuseum.ui.components.BottomBarApp
+import com.example.metmuseum.ui.components.SideBarApp
 import com.example.metmuseum.ui.homeScreen.HomeScreen
 import com.example.metmuseum.ui.theme.MetMuseumTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val windowSizeClass = calculateWindowSizeClass(this)
             MetMuseumTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ScaffoldExample()
+                    MetMuseumApp(windowSizeClass)
                 }
             }
         }
     }
 }
 
+@Composable
+fun MetMuseumApp(windowSize: WindowSizeClass) {
+    when (windowSize.widthSizeClass) {
+        WindowWidthSizeClass.Compact -> {
+            MetMuseumAppPortrait()
+        }
+
+        WindowWidthSizeClass.Expanded -> {
+            MetMuseumAppLandscape()
+        }
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScaffoldExample() {
+fun MetMuseumAppPortrait() {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -67,16 +75,23 @@ fun ScaffoldExample() {
             )
         },
         bottomBar = {
-                BottomBarApp()
-
+            BottomBarApp()
         },
     ) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
         ) {
             HomeScreen()
         }
+    }
+}
+
+@Composable
+fun MetMuseumAppLandscape() {
+    Row{
+        SideBarApp()
+        HomeScreen()
     }
 }
--- app/src/main/java/com/example/metmuseum/ui/artScreen/ArtScreen.kt
+++ app/src/main/java/com/example/metmuseum/ui/artScreen/ArtScreen.kt
@@ -1,19 +1,12 @@
 package com.example.metmuseum.ui.artScreen
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.metmuseum.model.Artpiece
-import com.example.metmuseum.ui.components.ArtCard
 import com.example.metmuseum.ui.components.ArtScreenColumn
 import com.example.metmuseum.ui.components.DepartmentGrid
 import com.example.metmuseum.ui.components.Searchbar
