9e3784ac441b14f1f355dd3883e09fab44b62c8d
Sebastiaan Delodder <sebastiaan.delodder@student.hogent.be>
2024-01-07 13:54:44 +0100

    Adding dokka documentation for model package

--- app/src/main/java/com/example/metmuseum/model/Department.kt
+++ app/src/main/java/com/example/metmuseum/model/Department.kt
@@ -1,5 +1,11 @@
 package com.example.metmuseum.model
 
+/**
+ * Data class representing a Department entity.
+ *
+ * @property departmentId The unique identifier for the department.
+ * @property displayName The display name of the department.
+ */
 data class Department (
     val departmentId: Int,
     val displayName: String,
