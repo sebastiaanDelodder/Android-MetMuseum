a1a5014fe19c47037327ad8d570dfaded35cf32e
Sebastiaan Delodder <sebastiaan.delodder@student.hogent.be>
2024-01-07 13:52:55 +0100

    Adding dokka documentation for data package

--- app/src/main/java/com/example/metmuseum/data/repository/ArtpiecesRepository.kt
+++ app/src/main/java/com/example/metmuseum/data/repository/ArtpiecesRepository.kt
@@ -17,54 +17,89 @@ import kotlinx.coroutines.flow.onEach
 import java.net.SocketTimeoutException
 
 /**
- * Repository that provides insert, update, delete, and retrieve of [Artpiece] from a given data source.
+ * Repository interface for managing [Artpiece] entities.
+ *
+ * This interface defines methods for retrieving, inserting, updating, deleting, and refreshing Artpieces.
  */
 interface ArtpiecesRepository {
     /**
-     * Retrieve all the artpieces from the the given data source.
+     * Retrieve all Artpieces from the given data source for the specified department.
+     *
+     * @param department The [Department] for which to retrieve Artpieces.
+     * @return A [Flow] emitting a list of [Artpiece] objects for the specified department.
      */
     fun getArtpieces(department: Department): Flow<List<Artpiece>>
 
     /**
-     * Retrieve an artpiece from the given data source that matches with the [id].
+     * Retrieve an [Artpiece] from the given data source that matches the specified [id].
+     *
+     * @param id The unique identifier of the [Artpiece] to retrieve.
+     * @return A [Flow] emitting the [Artpiece] with the specified [id], or null if not found.
      */
     fun getArtpiece(id: Int): Flow<Artpiece?>
 
     /**
-     * Insert artpiece in the data source
+     * Insert a new [Artpiece] into the local database.
+     *
+     * @param artpiece The Artpiece to be inserted.
      */
     suspend fun insertArtpiece(artpiece: Artpiece)
 
     /**
-     * Delete artpiece from the data source
+     * Delete an [Artpiece] from the local database.
+     *
+     * @param artpiece The Artpiece to be deleted.
      */
     suspend fun deleteArtpiece(artpiece: Artpiece)
 
     /**
-     * Update artpiece in the data source
+     * Update an existing [Artpiece] in the local database.
+     *
+     * @param artpiece The Artpiece to be updated.
      */
     suspend fun updateArtpiece(artpiece: Artpiece)
 
     /**
-     * Refresh the artpieces stored in the data source
+     * Refresh the Artpiece object identifiers stored in the data source for the specified department.
+     *
+     * @param departmentId The unique identifier of the [Department] for which to refresh Artpieces.
+     * @return A [Flow] emitting a list of unique identifiers of refreshed Artpieces.
      */
     suspend fun refresh(departmentId : Int) : Flow<List<Int>>
 
     /**
-     * Refresh the artpieces stored in the data source
+     * Refresh a specific [Artpiece] stored in the data source.
+     *
+     * @param objectId The unique identifier of the Artpiece to refresh.
      */
     suspend fun refreshArtPiece(objectId : Int)
 }
 
+/**
+ * Implementation of the [ArtpiecesRepository] interface with caching functionality.
+ *
+ * This class uses a local database access object ([ArtpieceDao]) and a remote API service ([ArtpieceApiService]) to manage Artpieces.
+ *
+ * @property artpieceDao Local database access object for Artpieces.
+ * @property artpieceApiService Remote API service for Artpieces.
+ */
 class CachingArtpiecesRepository(
     private val artpieceDao: ArtpieceDao,
     private val artpieceApiService: ArtpieceApiService
 ): ArtpiecesRepository {
 
-    //this repo contains logic to refresh the departments (remote)
-    //sometimes that logic is written in a 'usecase'
+    /**
+     * Retrieve all Artpieces from the given data source for the specified department.
+     *
+     * The department's display name is optionally corrected based on predefined mappings.
+     * This is due conflicting names stored in the original data source.
+     * The flow is observed for changes, and if no Artpieces are cached, a refresh is triggered.
+     *
+     * @param department The [Department] for which Artpieces are requested.
+     * @return A [Flow] emitting a list of [Artpiece] objects for the specified department.
+     */
     override fun getArtpieces(department: Department): Flow<List<Artpiece>> {
-        //TODO
+        Log.i("CachingArtpiecesRepo", "getArtpieces for: $department")
         var stringCorrection = if (department.displayName == "American Decorative Arts") {
             "The American Wing"
         } else if (department.displayName == "Arts of Africa, Oceania, and the Americas") {
@@ -82,47 +117,79 @@ class CachingArtpiecesRepository(
         return artpieceDao.getAllArtpieces(stringCorrection).map {
             it.asDomainArtpieces()
         }.onEach {
-            //Log.i("CachingArtpiecesRepo", "getArtpieces: $it")
-            //todo: check when refresh is called (why duplicates??)
             if (it.isEmpty()) {
                 refresh(department.departmentId)
             }
         }
     }
 
+    /**
+     * Retrieve an [Artpiece] from the given data source that matches the specified [id].
+     *
+     * @param id The ID of the [Artpiece] to retrieve.
+     * @return A [Flow] emitting the [Artpiece] with the specified ID, or null if not found.
+     */
     override fun getArtpiece(id: Int): Flow<Artpiece?> {
         return artpieceDao.getArtpiece(id).map {
             it.asDomainArtpiece()
         }
     }
 
+    /**
+     * Inserts a new [Artpiece] into the local database.
+     *
+     * @param artpiece The [Artpiece] to be inserted.
+     */
     override suspend fun insertArtpiece(artpiece: Artpiece) {
         artpieceDao.insert(artpiece.asDbArtpiece())
     }
 
+    /**
+     * Deletes an [Artpiece] from the local database.
+     *
+     * @param artpiece The [Artpiece] to be deleted.
+     */
     override suspend fun deleteArtpiece(artpiece: Artpiece) {
         artpieceDao.delete(artpiece.asDbArtpiece())
     }
 
+    /**
+     * Updates an existing [Artpiece] in the local database.
+     *
+     * @param artpiece The [Artpiece] to be updated.
+     */
     override suspend fun updateArtpiece(artpiece: Artpiece) {
         artpieceDao.update(artpiece.asDbArtpiece())
     }
 
+    /**
+     * Initiates a refresh of Artpieces data for a specific department.
+     *
+     * The refresh is performed by querying a remote API for Artpieces related to the specified department.
+     *
+     * @param departmentId The ID of the [Department] to refresh.
+     * @return A [Flow] emitting a list of IDs for the refreshed Artpieces.
+     */
     override suspend fun refresh(departmentId: Int): Flow<List<Int>> {
         try {
             Log.i("CachingArtpiecesRepository", "Refresh: departmentId = $departmentId")
             return artpieceApiService.getArtpiecesAsFlow(departmentId)
         } catch (e: SocketTimeoutException) {
-            //log something
-            //TODO
-            Log.e("CachingArtpiecesRepository", "Refresh: departmentId = $departmentId Error:" + e.stackTraceToString(),)
+            Log.e("CachingArtpiecesRepository", "Refresh Error: " + e.stackTraceToString(),)
             throw e
         } catch (e: Exception) {
-            Log.e("CachingArtpiecesRepository", "Refresh: departmentId = $departmentId Error:" + e.stackTraceToString(),)
+            Log.e("CachingArtpiecesRepository", "Refresh Error: " + e.stackTraceToString(),)
             throw e
         }
     }
 
+    /**
+     * Initiates a refresh of a specific [Artpiece] data.
+     *
+     * The refresh is performed by querying a remote API for the specific Artpiece with the given object ID.
+     *
+     * @param objectId The object ID of the [Artpiece] to refresh.
+     */
     override suspend fun refreshArtPiece(objectId: Int) {
         try {
             artpieceApiService.getArtpieceAsFlow(objectId).asDomainObject().collect { value ->
@@ -130,9 +197,7 @@ class CachingArtpiecesRepository(
                 insertArtpiece(value)
             }
         } catch (e: SocketTimeoutException) {
-            //log something
-            //TODO
-            Log.e("CachingArtpiecesRepository", "Refresh: Artpiece Error: " + e.stackTraceToString(),)
+            Log.e("CachingArtpiecesRepository", "RefreshArtpiece: Error: " + e.stackTraceToString(),)
         }
     }
 }
--- app/src/main/java/com/example/metmuseum/data/repository/DepartmentsRepository.kt
+++ app/src/main/java/com/example/metmuseum/data/repository/DepartmentsRepository.kt
@@ -15,93 +15,142 @@ import kotlinx.coroutines.flow.onEach
 import java.net.SocketTimeoutException
 
 /**
- * Repository that provides insert, update, delete, and retrieve of [Department] from a given data source.
+ * Repository interface for managing [Department] entities.
+ *
+ * This interface defines methods for retrieving, inserting, updating, deleting, and refreshing Departments.
  */
 interface DepartmentsRepository {
     /**
-     * Retrieve all the departments from the the given data source.
+     * Retrieve all Departments from the given data source.
+     *
+     * @return A [Flow] emitting a list of [Department] objects.
      */
     fun getDepartments(): Flow<List<Department>>
 
     /**
-     * Retrieve a department from the given data source that matches with the [id].
+     * Retrieve a [Department] from the given data source that matches the specified [id].
+     *
+     * @param id The unique identifier of the [Department] to retrieve.
+     * @return A [Flow] emitting the [Department] with the specified [id], or null if not found.
      */
     fun getDepartment(id: Int): Flow<Department?>
 
     /**
-     * Insert department in the data source
+     * Insert a new [Department] into the local database.
+     *
+     * @param department The [Department] to be inserted.
      */
     suspend fun insertDepartment(department: Department)
 
     /**
-     * Delete department from the data source
+     * Delete a [Department] from the local database.
+     *
+     * @param department The [Department] to be deleted.
      */
     suspend fun deleteDepartment(department: Department)
 
     /**
-     * Update department in the data source
+     * Update an existing [Department] in the local database.
+     *
+     * @param department The [Department] to be updated.
      */
     suspend fun updateDepartment(department: Department)
 
     /**
-     * Refresh the departments stored in the data source
+     * Refresh the departments stored in the data source.
      */
     suspend fun refresh()
 }
 
+/**
+ * Implementation of the [DepartmentsRepository] interface with caching functionality.
+ *
+ * This class uses a local database access object ([DepartmentDao]) and a remote API service ([DepartmentApiService]) to manage Departments.
+ *
+ * @property departmentDao Local database access object for Departments.
+ * @property departmentApiService Remote API service for Departments.
+ */
 class CachingDepartmentsRepository(
     private val departmentDao: DepartmentDao,
     private val departmentApiService: DepartmentApiService
 ): DepartmentsRepository {
 
-    //this repo contains logic to refresh the departments (remote)
-    //sometimes that logic is written in a 'usecase'
+    /**
+     * Retrieve all Departments from the given data source.
+     *
+     * The flow is observed for changes, and if no Departments are cached, a refresh is triggered.
+     *
+     * @return A [Flow] emitting a list of [Department] objects.
+     */
     override fun getDepartments(): Flow<List<Department>> {
+        Log.i("CachingDepartmentsRepository", "getDepartments")
         return departmentDao.getAllDepartments().map {
             it.asDomainDepartments()
         }.onEach {
-            //todo: check when refresh is called (why duplicates??)
             if(it.isEmpty()){
-                Log.i("CachingDepartmentsRepository", "getDepartments")
                 refresh()
             }
         }
     }
 
+    /**
+     * Retrieve a [Department] from the given data source that matches the specified [id].
+     *
+     * @param id The ID of the [Department] to retrieve.
+     * @return A [Flow] emitting the [Department] with the specified ID, or null if not found.
+     */
     override fun getDepartment(id: Int): Flow<Department?> {
         return departmentDao.getDepartment(id).map {
             it.asDomainDepartment()
         }
     }
 
+    /**
+     * Inserts a new [Department] into the local database.
+     *
+     * @param department The [Department] to be inserted.
+     */
     override suspend fun insertDepartment(department: Department) {
         departmentDao.insert(department.asDbDepartment())
     }
 
+    /**
+     * Deletes a [Department] from the local database.
+     *
+     * @param department The [Department] to be deleted.
+     */
     override suspend fun deleteDepartment(department: Department) {
         departmentDao.delete(department.asDbDepartment())
     }
 
+    /**
+     * Updates an existing [Department] in the local database.
+     *
+     * @param department The [Department] to be updated.
+     */
     override suspend fun updateDepartment(department: Department) {
         departmentDao.update(department.asDbDepartment())
     }
 
+    /**
+     * Initiates a refresh of a specific [Department] data.
+     *
+     * The refresh is performed by querying a remote API for the specific Department with the given object ID.
+     *
+     * @param objectId The object ID of the [Department] to refresh.
+     */
     override suspend fun refresh(){
         try {
             departmentApiService.getDepartmentsAsFlow().asDomainObjects().collect {
                     value ->
                 Log.i("CachingDepartmentsRepository", "Refresh: $value")
-                Log.i("CachingDepartmentsRepository", "Refresh: inserting departments")
                 for(department in value) {
                     insertDepartment(department)
                 }
-
             }
         }
         catch(e: SocketTimeoutException){
-            //log something
-            //TODO
-            Log.e("CachingDepartmentsRepository", "Refresh: " +e.stackTraceToString(), )
+            Log.e("CachingDepartmentsRepository", "Refresh: Error: " +e.stackTraceToString(), )
         }
     }
 }
